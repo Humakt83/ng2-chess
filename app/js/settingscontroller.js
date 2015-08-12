6b3abebfecd6a1eaaa9cdbf886ef230b41fc04d6
Humakt83 <urlact@yahoo.com>
2015-08-13 01:04:22 +0300

    Depth and moves to evaluate selections for each AI

--- app/js/settingscontroller.js
+++ app/js/settingscontroller.js
@@ -4,7 +4,12 @@ angular.module('ng-chess').controller('SettingsController', ['$scope', '$locatio
 	
 	$scope.settings = Settings
 	
+	$scope.difficultyWhite = Settings.getDifficultyWhite()
+	$scope.difficultyBlack = Settings.getDifficultyBlack()
+	
 	$scope.start = function() {
+		Settings.setDifficultyWhite($scope.difficultyWhite.depth, $scope.difficultyWhite.width)
+		Settings.setDifficultyBlack($scope.difficultyBlack.depth, $scope.difficultyBlack.width)
 		$location.path('chess')
 	}
 	
