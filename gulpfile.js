82bf097521cbedd0af386ea0b864ebcefc7896d0
Humakt83 <urlact@yahoo.com>
2015-08-15 13:40:12 +0300

    Updated build script and added icon

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ var assign = require('lodash.assign');
 var browserSync = require('browser-sync');
 var uglify = require('gulp-uglify');
 
+var DIST_DIR = './dist/'
 // add custom browserify options here
 var customOpts = {
   entries: ['./app/ngchess.js'],
@@ -25,13 +26,32 @@ gulp.task('copyFiles', function() {
 	gulp.src([
         './app/index.html',
         './app/style.css',
+		'./app/ngchess.ico',
         './app/img/**/*',
 		'./app/partials/**/*'        
     ])
-	.pipe(gulp.dest('dist/'));
+	.pipe(gulp.dest(DIST_DIR));
 })
+
 gulp.task('default', ['copyFiles'], bundle); // so you can run `gulp js` to build the file
 gulp.task('serve', ['default'], function() {
+	browserSync({
+		server: {
+			baseDir: 'dist'
+		}
+	});
+});
+
+gulp.task('dist', ['copyFiles'], function() {
+	return browserify(assign({}, customOpts))
+		.bundle()
+		.pipe(source('bundle.js'))
+		.pipe(buffer())		
+		.pipe(uglify())
+		.pipe(gulp.dest(DIST_DIR));
+});
+
+gulp.task('serve-dist', ['dist'], function() {
 	browserSync({
 		server: {
 			baseDir: 'dist'
@@ -45,10 +65,9 @@ b.on('log', gutil.log); // output build logs to terminal
 function bundle() {
   return b.bundle()
     .on('error', gutil.log.bind(gutil, 'Browserify Error'))
-    .pipe(source('bundle.min'))
+    .pipe(source('bundle.js'))
     .pipe(buffer())
     .pipe(sourcemaps.init({loadMaps: true}))
-	//.pipe(uglify())
     .pipe(sourcemaps.write('./'))	
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest(DIST_DIR));
 }
