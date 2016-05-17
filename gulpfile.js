9faede2f7da755ab1c4e1eb18a89716d71f527fa
Humakt83 <urlact@yahoo.com>
2016-05-18 00:03:30 +0300

    Updated build

--- gulpfile.js
+++ gulpfile.js
@@ -1,75 +1,51 @@
-'use strict';
+const gulp = require('gulp');
+const del = require('del');
+const typescript = require('gulp-typescript');
+const tscConfig = require('./tsconfig.json');
+const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync');
+const reload = browserSync.reload;
 
-var watchify = require('watchify');
-var browserify = require('browserify');
-var gulp = require('gulp');
-var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
-var gutil = require('gulp-util');
-var sourcemaps = require('gulp-sourcemaps');
-var assign = require('lodash.assign');
-var browserSync = require('browser-sync');
-var uglify = require('gulp-uglify');
-var babelify = require('babelify');
+gulp.task('clean', function () {
+    return del('dist/**/*');
+});
 
-var DIST_DIR = './dist/'
-// add custom browserify options here
-var customOpts = {
-  entries: ['./app/ngchess.js'],
-  extensions: ['.js'],
-  transform: [babelify],
-  paths: ['./node_modules','./app/js'],
-  debug: true
-};
-var opts = assign({}, watchify.args, customOpts);
-var b = watchify(browserify(opts)); 
+gulp.task('compile', ['clean'], function () {
+    return gulp
+        .src('src/**/*.ts')
+        .pipe(typescript(tscConfig.compilerOptions))
+        .pipe(gulp.dest('dist'));
+});
 
-gulp.task('copyFiles', function() {
-	gulp.src([
-        './app/index.html',
-        './app/style.css',
-		'./app/ngchess.ico',
-        './app/img/**/*',
-		'./app/partials/**/*'        
+gulp.task('copy:libs', ['clean'], function() {
+    return gulp.src([
+        'node_modules/angular2/bundles/angular2-polyfills.min.js',
+        'node_modules/systemjs/dist/system.src.js',
+        'node_modules/systemjs/dist/system.js',
+        'node_modules/rxjs/bundles/Rx.min.js',
+        'node_modules/angular2/bundles/angular2.min.js',
+        'node_modules/es6-shim/es6-shim.min.js',
+        'node_modules/systemjs/dist/system-polyfills.js',
+		'node_modules/lodash/lodash.js',
+		'node_modules/jschessrulz/index.js'
     ])
-	.pipe(gulp.dest(DIST_DIR));
-})
-
-gulp.task('default', ['copyFiles'], bundle); // so you can run `gulp js` to build the file
-gulp.task('serve', ['default'], function() {
-	browserSync({
-		server: {
-			baseDir: 'dist'
-		}
-	});
+        .pipe(gulp.dest('dist/lib'))
 });
 
-gulp.task('dist', ['copyFiles'], function() {
-	return browserify(assign({}, customOpts))
-		.bundle()
-		.pipe(source('bundle.js'))
-		.pipe(buffer())		
-		.pipe(uglify())
-		.pipe(gulp.dest(DIST_DIR));
+gulp.task('copy:assets', ['clean'], function() {
+    return gulp.src(['src/**/*', '!src/**/*.ts'])
+        .pipe(gulp.dest('dist'))
 });
 
-gulp.task('serve-dist', ['dist'], function() {
-	browserSync({
-		server: {
-			baseDir: 'dist'
-		}
-	});
+gulp.task('serve', ['build'], function() {
+    browserSync({
+        server: {
+            baseDir: 'dist'
+        }
+    });
+    gulp.watch(['src/**/*'], ['buildAndReload']);
 });
 
-b.on('update', bundle); // on any dep update, runs the bundler
-b.on('log', gutil.log); // output build logs to terminal
-
-function bundle() {
-  return b.bundle()
-    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
-    .pipe(source('bundle.js'))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({loadMaps: true}))
-    .pipe(sourcemaps.write('./'))	
-    .pipe(gulp.dest(DIST_DIR));
-}
+gulp.task('build', ['compile', 'copy:libs', 'copy:assets']);
+gulp.task('buildAndReload', ['build'], reload);
+gulp.task('default', ['build']);
